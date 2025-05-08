6b809d709e6805cd5dafe25cfd861ecb52df8419
Flex224 <flexon46@gmail.com>
2025-05-08 18:26:13 +0300

    Add files via upload

---
+++ last_paid_click.sql
@@ -0,0 +1,35 @@
+WITH last_paid_click AS (
+    SELECT DISTINCT ON (s.visitor_id)
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    FROM
+        sessions AS s
+    LEFT JOIN
+        leads AS l
+        ON s.visitor_id = l.visitor_id
+        AND s.visit_date <= l.created_at
+    WHERE
+        s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    ORDER BY 
+        s.visitor_id, s.visit_date DESC
+)
+SELECT *
+FROM 
+	last_paid_click 
+ORDER BY
+    amount DESC NULLS LAST,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign
+LIMIT 10;
+	
+
