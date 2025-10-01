003c3abbcfce816eed09c131d00537b0a12b415d
Flex224 <flexon46@gmail.com>
2025-10-02 00:07:14 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -15,14 +15,15 @@ WITH visitors_with_leads AS (
         ON s.visitor_id = l.visitor_id
         AND s.visit_date <= l.created_at
     WHERE s.medium != 'organic'
-    ORDER BY s.visitor_id ASC, s.visit_date DESC
+    ORDER BY s.visitor_id, s.visit_date DESC
 ),
-utm_aggregate AS (
+
+utm_aggregates AS (
     SELECT
         utm_source,
         utm_medium,
         utm_campaign,
-        CAST(visit_date AS date) AS visit_date,
+        DATE(visit_date) AS visit_date,
         COUNT(visitor_id) AS visitors_count,
         COUNT(
         	CASE 
@@ -32,12 +33,9 @@ utm_aggregate AS (
         COUNT(CASE WHEN status_id = 142 THEN visitor_id END) AS purchases_count,
         SUM(CASE WHEN status_id = 142 THEN amount END) AS revenue
     FROM visitors_with_leads
-    GROUP BY
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        visit_date
+    GROUP BY 1, 2, 3, 4
 ),
+
 ad_costs AS (
     SELECT
         DATE(campaign_date) AS visit_date,
@@ -46,11 +44,7 @@ ad_costs AS (
         utm_campaign,
         SUM(daily_spent) AS total_cost
     FROM ya_ads
-    GROUP BY
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign
+    GROUP BY 1, 2, 3, 4
     UNION ALL
     SELECT
         DATE(campaign_date) AS visit_date,
@@ -59,33 +53,31 @@ ad_costs AS (
         utm_campaign,
         SUM(daily_spent) AS total_cost
     FROM vk_ads
-    GROUP BY
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign
+    GROUP BY 1, 2, 3, 4
 )
+
 SELECT
     u.visit_date,
     u.visitors_count,
     u.utm_source,
     u.utm_medium,
     u.utm_campaign,
-    a.total_cost,
+    a.total_cost,  
     u.leads_count,
     u.purchases_count,
-    u.revenue
-FROM utm_aggregate AS u
+    u.revenue  
+FROM utm_aggregates AS u
 LEFT JOIN ad_costs AS a
     ON u.visit_date = a.visit_date
     AND u.utm_source = a.utm_source
     AND u.utm_medium = a.utm_medium
     AND u.utm_campaign = a.utm_campaign
 ORDER BY
-    u.revenue DESC NULLS LAST,
-    u.visit_date ASC,
-    u.utm_source DESC,
-    u.utm_medium ASC,
-    u.utm_campaign ASC,
-    u.visitors_count ASC
+    revenue DESC NULLS LAST,
+    visit_date ASC,
+    utm_source DESC,
+    utm_medium ASC,
+    utm_campaign ASC,
+    visitors_count ASC
 LIMIT 15;
+
