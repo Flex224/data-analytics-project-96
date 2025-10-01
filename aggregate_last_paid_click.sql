72066d99d6f4f9cf9049189c1f07453d3bb16e51
Flex224 <flexon46@gmail.com>
2025-10-02 01:08:13 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -11,10 +11,12 @@ WITH visitors_with_leads AS (
         s.campaign AS utm_campaign,
         s.source AS utm_source
     FROM
-            sessions AS s
+        sessions AS s
     LEFT JOIN leads AS l
-        ON s.visitor_id = l.visitor_id
-        AND s.visit_date <= l.created_at
+        ON 
+            s.visitor_id = l.visitor_id
+        AND 
+     		s.visit_date <= l.created_at
     WHERE s.medium != 'organic'
     ORDER BY s.visitor_id ASC, s.visit_date DESC
 ),
@@ -34,7 +36,7 @@ utm_aggregates AS (
         COUNT(CASE WHEN status_id = 142 THEN visitor_id END) AS purchases_count,
         SUM(CASE WHEN status_id = 142 THEN amount END) AS revenue
     FROM visitors_with_leads
-    GROUP BY 1, 2, 3, 4
+    GROUP BY utm_source, utm_medium, utm_campaign, visit_date
 ),
 
 ad_costs AS (
@@ -54,31 +56,31 @@ ad_costs AS (
         utm_campaign,
         SUM(daily_spent) AS total_cost
     FROM vk_ads
-    GROUP BY 1, 2, 3, 4
+    GROUP BY utm_source, utm_medium, utm_campaign, visit_date
 )
 
 SELECT
-        u.visit_date,
-        u.visitors_count,
-        u.utm_source,
-        u.utm_medium,
-        u.utm_campaign,
-        a.total_cost,
-        u.leads_count,
-        u.purchases_count,
-        u.revenue
+    u.visit_date,
+    u.visitors_count,
+    u.utm_source,
+    u.utm_medium,
+    u.utm_campaign,
+    a.total_cost,
+    u.leads_count,
+    u.purchases_count,
+    u.revenue
 FROM
-        utm_aggregates AS u
+    utm_aggregates AS u
 LEFT JOIN ad_costs AS a
     ON u.visit_date = a.visit_date
     AND u.utm_source = a.utm_source
     AND u.utm_medium = a.utm_medium
     AND u.utm_campaign = a.utm_campaign
 ORDER BY
-        u.revenue DESC NULLS LAST,
-        u.visit_date ASC,
-        u.utm_source DESC,
-        u.utm_medium ASC,
-        u.utm_campaign ASC,
-        u.visitors_count ASC
+    u.revenue DESC NULLS LAST,
+    u.visit_date ASC,
+    u.utm_source DESC,
+    u.utm_medium ASC,
+    u.utm_campaign ASC,
+    u.visitors_count ASC
 LIMIT 15;
